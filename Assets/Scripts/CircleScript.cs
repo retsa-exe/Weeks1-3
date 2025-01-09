9c5e4347ef9c353b81803897f1fb984916368244
Aster Lin <asterlin7890@gmail.com>
2025-01-09 15:10:44 -0500

    Make the circle attached to the mouse And fix the quare movement

--- Assets/Scripts/CircleScript.cs
+++ Assets/Scripts/CircleScript.cs
@@ -2,21 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstScript : MonoBehaviour
+public class CircleScript : MonoBehaviour
 {
-    float speed = 0.1f;
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 pos = transform.position;
-        pos.x += speed;
-        transform.position = pos;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
     }
 }
