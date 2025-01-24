3fdf8c25fd514bcc77194419482efe9bebbb3581
LIN/windows <asterlin7890@gmail.com>
2025-01-24 15:15:24 -0500

    merge into main

--- Assets/Assignment 1/Scripts/followTheMouse.cs
+++ Assets/Assignment 1/Scripts/followTheMouse.cs
@@ -13,7 +13,9 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //transform the mouse position to world point
         Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //set the position to the mouse poistion
         transform.position = pos;
     }
 }
