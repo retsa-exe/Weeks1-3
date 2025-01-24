b88e0d2581e4a103725418582570ee99d4b11f21
LIN/windows <asterlin7890@gmail.com>
2025-01-24 15:21:30 -0500

    create lip script

---
+++ Assets/Assignment 1/Scripts/lipControl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lipControl : MonoBehaviour
+{
+    public bool lip = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get mouse position
+        Vector2 pos = Input.mousePosition;
+
+        //detect if mouse clicks on the lip
+    }
+}
