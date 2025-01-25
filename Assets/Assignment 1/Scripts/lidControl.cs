b144da3c6f8a37de6a86a2cf60ff8b592eac88da
LIN/windows <asterlin7890@gmail.com>
2025-01-25 10:35:35 -0500

    add lid animation

--- Assets/Assignment 1/Scripts/lidControl.cs
+++ Assets/Assignment 1/Scripts/lidControl.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class lidControl : MonoBehaviour
 {
+    //the state of the lid
     public bool lid = false;
+    //the speed of the lid animation
+    public float lidSpeed = 2f;
+    //where are the lid going
+    Vector2 targetPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //close the lid when start
+        targetPos = new Vector2(0,0);
+        transform.position = targetPos;
     }
 
     // Update is called once per frame
@@ -25,15 +32,17 @@ public class lidControl : MonoBehaviour
             lid = ! lid;
         }
 
-        //show the lid when true
+        //change the target position
         if (lid)
         {
-            transform.position = new Vector2(0, 0);
+            targetPos = new Vector2(0, 0);
         }
         else
         {
-            //remove the lid from screen if false
-            transform.position = new Vector2(0, 7);
+            targetPos = new Vector2(0, 7);
         }
+
+        //move the lid to the target position
+        transform.position = Vector2.Lerp(transform.position, targetPos, Time.deltaTime * lidSpeed);
     }
 }
